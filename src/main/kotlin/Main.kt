0e2a0bdf1c691a92a72c5e27da00edd50f6b49e7
Roman Avanesyan <creker88@rambler.ru>
2023-05-10 15:00:31 +0300

    correct fun calcCommision commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,30 @@
 fun main() {
-    val commissionSize = calcCommission(20000)
-
-    println("Комиссия составит: $commissionSize руб.")
+    val commissionSize = calcCommission(1000000,"МИР" )
+    if (commissionSize < 0) {
+        println("Ошибка операции: $commissionSize")
+    } else
+        println("Комиссия составит: $commissionSize руб.")
 }
 
 fun calcCommission(amountOfTransfer: Int, typeOfCard: String = "VK Pay", amountOfPreviousTransfer: Int = 0): Int {
-    var commission = 0
+    val commission = when {
+        (typeOfCard == "VK Pay") -> {
+            if (amountOfTransfer > 15_000 || (amountOfTransfer + amountOfPreviousTransfer) > 40_000) -1 else 0
+        }
 
-    if (typeOfCard == "VK Pay") {
-        commission = if (amountOfTransfer > 15_000 || (amountOfTransfer + amountOfPreviousTransfer) > 40_000) -1 else 0
-    } else if (amountOfTransfer > 150_000 || (amountOfTransfer + amountOfPreviousTransfer) > 600_000) {
-        commission = -2
-    } else if (typeOfCard == "Mastercard" || typeOfCard == "Maestro") {
-        commission = if (amountOfTransfer > 75_000) (amountOfTransfer * 0.6 / 100).toInt() + 20 else 0
-    } else if (typeOfCard == "Visa" || typeOfCard == "МИР") {
-        commission = if ((amountOfTransfer * 0.75 / 100).toInt() > 35) (amountOfTransfer * 0.75 / 100).toInt() else 35
+        (amountOfTransfer > 150_000 || (amountOfTransfer + amountOfPreviousTransfer) > 600_000) -> {
+            -2
+        }
+
+        (typeOfCard == "Mastercard" || typeOfCard == "Maestro") -> {
+            if (amountOfTransfer + amountOfPreviousTransfer > 75_000) (amountOfTransfer * 0.6 / 100).toInt() + 20 else 0
+        }
+
+        (typeOfCard == "Visa" || typeOfCard == "МИР") -> {
+            if ((amountOfTransfer * 0.75 / 100).toInt() > 35) (amountOfTransfer * 0.75 / 100).toInt() else 35
+        }
+
+        else -> -3
     }
     return commission
 }
